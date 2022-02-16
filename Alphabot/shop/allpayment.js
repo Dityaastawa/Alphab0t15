19be71e447cdbd02a3f3cd2f26c46b2554c9bc53
Dityaastawa <dityaastawa2@gmail.com>
2022-02-16 08:31:34 +0800

    Alphab0t15

---
+++ Alphabot/shop/allpayment.js
@@ -0,0 +1,35 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY DITYA ASTAWA
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ -
+2. BCA 
+	ㅁ -
+3. BNI 
+	ㅁ -
+4. PERMATA
+	ㅁ -
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ -
+2. OVO
+	ㅁ -
+3. DANA
+	ㅁ 0857 9242 9140
+	
+Semuanya atas nama DI****AS
+
+*📲 PULSA IM3*
+
+085 792 429 140
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ Alphabot/shop/grupbot.js
@@ -0,0 +1,14 @@
+// jangan di ubah jika tidak punya group bot, takutnya eror
+const gcbotwa = () =>{
+	return`Join Aja Kak Bantu Ramaikan !
+
+1. *DITYA BOTZ GROUP*
+ https://chat.whatsapp.com/LJHLvvu63DMG9MNBqua30M
+2. *DITYA BOTZ GROUP 2*
+ https://chat.whatsapp.com/EXYsgUhiKy38TguvDcLyko
+
+Jika ada link yang ke reset, silahkan hubungi
+owner untuk meminta link yang baru
+`
+}
+exports.gcbotwa = gcbotwa
